a87df386786afc39b410e486ab63b9ade008f29c
Annie <annmora21@gmail.com>
2025-11-30 17:45:34 -0800

    Update

--- 210-lab-38-starter.cpp
+++ 210-lab-38-starter.cpp
@@ -38,35 +38,41 @@ int main() {
     // Menu
     int sel = main_menu();
     string code;
-    while (sel != 5) {
+    while (sel != 7) {
         switch (sel) {
         
             case 1:
                 cout << "You chose to add to the record.\n";
                 cout << "Type in a code (8 digits containing letters and numbers only): ";
-                tree.insertNode(code);
+                getline(cin, code);
+                tree.insertNode(code); // TESTING1
                 break;
             case 2:
                 cout << "You chose to delete from the record.\n";
                 cout << "Enter a code to delete (8 digits containing letters and numbers only): ";
-                tree.remove(code);
+                getline(cin, code);
+                tree.remove(code); // zzzvpPa2
                 break;
             case 3:
                 cout << "You chose to search the record.\n";
                 cout << "Type in a code to search for (8 digits containing letters and numbers only): ";
+                getline(cin, code);
                 tree.searchNode(code);
                 break;
             case 4:
                 cout << "You chose to modify the record.\n";
                 // code
                 break;
+            case 5:
+                cout << "You chose to display the record.\n";
+                tree.displayInOrder();
+                sel = main_menu();
+                break;
+            case 6:
+                cout << "You chose to exit the program. See you next time!\n";
+                return 1;
         }
     }
-
-    /*
-    cout << "\nDisplaying current codes in the tree:\n";
-    tree.displayInOrder(); cout << endl;
-    */
 
     cout << "\n1. Project is set up and running using the provided files.";
     cout << "\n2. Code modification from int to strings is complete, and code exercises this for testing.";
@@ -80,11 +86,13 @@ int main_menu() {
     cout << "[2] Delete from the record\n";
     cout << "[3] Search the record\n";
     cout << "[4] Modify the record\n";
+    cout << "[5] Display record\n";
+    cout << "[6] Exit the program\n";
     cout << "Enter your choice -> ";
     int choice;
     cin >> choice; cout << endl;
-    while (choice < 0 || choice > 4) {
-        cout << "Invalid input, only select a number between 1-4: ";
+    while (choice < 0 || choice > 6) {
+        cout << "Invalid input, only select a number between 1-6: ";
         cin >> choice;
     }
     return choice;
